d14e09e862a7e550e3ef766aba8d1560b4802c10
SereneMeow95 <66867156+SereneMeow95@users.noreply.github.com>
2020-11-04 23:17:06 +0800

    15. Using Postman to signup user

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 //import express and store it in variable called app
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
+
+//import routes
+const userRoutes = require('./routes/user');
 
 //app
 const app = express();
@@ -9,18 +15,21 @@ const app = express();
 //db
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-    //useCreateIndex: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
 }).then(() => console.log("DB Connected"));
 
 mongoose.connection.on('error', err => {
     console.log(`DB connection error: ${err.message}`)
 });
 
-//the routes to handle the request from the homepage
-app.get('/', (req, res) => {
-    res.send("hello from node yay");
-});
+//middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+//the routes middleware
+app.use("/api", userRoutes);
 
 //port that run the server
 const port = process.env.PORT || 8000;
