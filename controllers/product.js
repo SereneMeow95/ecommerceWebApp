13ac3e272e67ec0d516d30b702698f0d30c5b021
SereneMeow95 <66867156+SereneMeow95@users.noreply.github.com>
2020-11-05 22:36:25 +0800

    31. Create product with file upload

---
+++ controllers/product.js
@@ -0,0 +1,33 @@
+const formidable = require('formidable');
+const _ = require("lodash");
+const Product = require('../models/product');
+const {errorHandler} = require('../helpers/dbErrorHandler');
+
+//create new product
+exports.create = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if(err) {
+            return res.status(400).json({
+                error: 'Image could not be uploaded.'
+            });
+        }
+        let product = new Product(fields);
+
+        if(files.photo) {
+            product.photo.data = fs.readFileSync(file.photo.path);
+            product.photo.contentType = files.photo.type;
+        }
+
+        product.save((err, result) => {
+            if(err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            res.json(result);
+        });
+    });
+};
+
