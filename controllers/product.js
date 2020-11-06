19e54b7c366d0826479da89d00b7c3de0f622367
SereneMeow95 <66867156+SereneMeow95@users.noreply.github.com>
2020-11-06 23:27:38 +0800

    45. Send product photo

--- controllers/product.js
+++ controllers/product.js
@@ -235,3 +235,12 @@ exports.listBySearch = (req, res) => {
             });
         });
 };
+
+//return product photo
+exports.photo = (req, res, next) => {
+    if (req.product.photo.data){
+        res.set('Content-Type', req.product.photo.contentType);
+        return res.send(req.product.photo.data);
+    }
+    next();
+};
