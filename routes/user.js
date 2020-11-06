f049cc81e4c5ebc7960b360eec8887c9d69b6abf
SereneMeow95 <66867156+SereneMeow95@users.noreply.github.com>
2020-11-06 23:43:25 +0800

    46. User profile read and update

--- routes/user.js
+++ routes/user.js
@@ -3,13 +3,18 @@ const router = express.Router();
 
 const {requireSignin, isAuth, isAdmin} = require('../controllers/auth');
 
-const {userById} = require('../controllers/user');
+const {userById, read, update} = require('../controllers/user');
 
 router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
     res.json({
         user: req.profile
     });
 });
+
+//see and update the profile
+router.get('/user/:userId', requireSignin, isAuth, read);
+router.put('/user/:userId', requireSignin, isAuth, update);
+
 
 //everytime theres a userID in the route parameter, this method will run and make user info available in the request object
 router.param('userId', userById);
