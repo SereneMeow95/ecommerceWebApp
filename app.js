148af2a1fb9842fbddc9777256be2f21be165e48
SereneMeow95 <66867156+SereneMeow95@users.noreply.github.com>
2020-11-06 23:45:59 +0800

    47. Installing CORS

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const expressValidator = require('express-validator');
 require('dotenv').config();
 
@@ -32,6 +33,7 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 
 //the routes middleware
 app.use("/api", authRoutes);
