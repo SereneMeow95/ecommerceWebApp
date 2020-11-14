0f8d0632310b0024cda60e9d5fcc06a093fd1cd7
SereneMeow95 <66867156+SereneMeow95@users.noreply.github.com>
2020-11-14 20:18:23 +0800

    144. User purchase history - Backend

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const {requireSignin, isAuth, isAdmin} = require('../controllers/auth');
 
-const {userById, read, update} = require('../controllers/user');
+const {userById, read, update, purchaseHistory} = require('../controllers/user');
 
 //const{sayHi} = require("../controllers/user");
 //router.get("/", sayHi);
@@ -17,6 +17,7 @@ router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
 //see and update the profile
 router.get('/user/:userId', requireSignin, isAuth, read);
 router.put('/user/:userId', requireSignin, isAuth, update);
+router.get('/orders/by/user/:userId', requireSignin, isAuth, purchaseHistory);
 
 
 //everytime theres a userID in the route parameter, this method will run and make user info available in the request object
